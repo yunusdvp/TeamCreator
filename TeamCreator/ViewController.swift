5066288d0568a00e7133c4b975539587c1e2927c
yunusdvp <yunusemre7033@gmail.com>
2024-07-26 21:10:19 +0300

    -updated2
    -updated2

--- TeamCreator/ViewController.swift
+++ TeamCreator/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Yunus")
+        print("Yunus Emre 1.Güncelleme")
     }
 
 
