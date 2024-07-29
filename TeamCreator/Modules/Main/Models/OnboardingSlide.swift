1c9ce8e29dc638b1f736676a05601534248d4cc0
Ceren Uludoğan <62713981+cerenuludogan@users.noreply.github.com>
2024-07-29 23:24:41 +0300

    update

---
+++ TeamCreator/Modules/Main/Models/OnboardingSlide.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingSlide.swift
+//  TeamCreator
+//
+//  Created by Ceren Uludoğan on 29.07.2024.
+//
+
+import Foundation
+import UIKit
+
+struct OnboardingSlide {
+    let title: String
+    let description: String
+    let image: UIImage
+}
