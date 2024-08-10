3f1e348a57add01d4af1d61336ed15e771fe3fad
yunusdvp <yunusemre7033@gmail.com>
2024-08-10 11:59:00 +0300

    -matchCreate updated
    The necessary algorithm was created to create equal teams when creating a match. Related functions were added

---
+++ TeamCreator/Models/Team.swift
@@ -0,0 +1,16 @@
+//
+//  Team.swift
+//  TeamCreator
+//
+//  Created by Yunus Emre ÖZŞAHİN on 10.08.2024.
+//
+
+import Foundation
+
+struct Team {
+    var players: [Player]
+
+    var totalSkillRating: Int {
+        return players.reduce(0) { $0 + ($1.skillRating ?? 0) }
+    }
+}
