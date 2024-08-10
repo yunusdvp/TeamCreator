a47b8e4d45cb8c98518af0f67a7f049440a5ab2f
yunusdvp <yunusemre7033@gmail.com>
2024-08-11 00:43:58 +0300

    layoutConstants added EntryViewController

--- TeamCreator/Modules/Entry/EntryViewController.swift
+++ TeamCreator/Modules/Entry/EntryViewController.swift
@@ -79,6 +79,9 @@ extension EntryViewController: EntryViewModelDelegate {
 
 //MARK: Layout Constants
 private extension EntryViewController{
-    
+    enum LayoutConstants {
+        static let itemSize = CGSize(width: UIScreen.main.bounds.width - 35, height: UIScreen.main.bounds.height / 5.25)
+        static let minimumLineSpacing: CGFloat = 20
+        static let sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
 }
-≈
