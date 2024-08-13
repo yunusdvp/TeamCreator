33a2d239958e022a41cbd6e6d52664a8de536dbb
Giray Aksu <109553036+girayaksu@users.noreply.github.com>
2024-08-13 08:00:41 +0300

    created MyMatches Screen

---
+++ TeamCreator/Modules/MyMatches/Cell/MyMatchesTableViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  MyMatchesTableViewCell.swift
+//  TeamCreator
+//
+//  Created by Giray Aksu on 13.08.2024.
+//
+
+import UIKit
+
+class MyMatchesTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var typeImage: UIImageView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var hourLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    func configure(with match: Match) {
+        dateLabel.text = match.dateFormatted
+        locationLabel.text = match.location
+        hourLabel.text = match.hourFormatted
+        typeLabel.text = match.sport
+        
+        // Seçilen spor dalına göre icon seçimi
+        switch SelectedSportManager.shared.selectedSport {
+        case .football:
+            typeImage.image = UIImage(named: "footballIcon2")
+        case .volleyball:
+            typeImage.image = UIImage(named: "volleyballIcon")
+        case .basketball:
+            typeImage.image = UIImage(named: "basketballIcon")
+        case .none:
+            typeImage.image = nil
+        }
+    }
+}
+
